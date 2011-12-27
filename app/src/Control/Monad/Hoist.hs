0164f5d186f777e9fc09298f8c0c6e635487addb
Daniel Waterworth <Da.Waterworth@gmail.com>
2011-12-27 08:05:07 +0000

    made having a MonadTrans instance a prerequisite to having a MonadHoist instance

--- app/src/Control/Monad/Hoist.hs
+++ app/src/Control/Monad/Hoist.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE ExistentialQuantification, RankNTypes #-}
+
 module Control.Monad.Hoist where
 
-class MonadHoist t where
+import Control.Monad.Trans
+
+class (MonadTrans t) => MonadHoist t where
   hoist :: (Monad m, Monad n) => (forall a. m a -> n a) -> t m a -> t n a
 
